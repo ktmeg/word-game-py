6b5d74f4a4c928ecf6cadd25ba60b4deecf9ee0c
Kat <kmneil2@gmail.com>
2020-02-21 13:50:59 -0500

    Add functions to classes

--- game.py
+++ game.py
@@ -1,45 +1,72 @@
 
-with open ('words.txt') as words:
-  lines = words 
+import random
+
+with open('words.txt', 'r') as f:
+  words = f.readlines()
+
+# class Words:
+
+#   def __init__(self, word):
+#     self.word = word 
+
+#   def __str__(self):
+#     word = print(random.choice(words))
 
 class Game:
   def __init__(self):
   # self.play_game = 
-    self.player = Player("Kat")
+    self.player = Player()
 
+  def print_word(self):
+    word = random.chice(words)
+    # s = set 
+    # while len(s)>0:
+    #     s.remove(random.choice(list(s)))
   
-  print(words.closed) 
+  def start_game(self, word):
+    self.word = print_word
 
-  def play(self):
-    pass
+  # def play(self):
 
-class Word:
-  def __init__(self):
-    print('init method')
-  
-  def __enter__(self):
-    print('entering word file')
 
-  def __exit__(self, exc_info):
-    print('exit words.txt')
 
-with open('words.txt', 'w') as f:
-  words = f.read()
-
-  
 class Player:
   def __init__(self, name):
     self.name = name
-    self.score = score 
+    self.score = 0 
     self.guess = guess
-
+  
   def __str__(self):
+    name = input('What is your name?')
     return f'{self.name}'
   
-  pass
+  def start_game(self, game):
+    choice = input('Hello, {self.name}. Are you ready to play?' (y)/(n))
+    if choice == 'y': start_game
 
 
+  def guess_letter(self):
+    guess = input('What letter would you like to guess?')
 
 
 game = Game()
 # game.play()
+
+
+
+
+
+# Can I use this to add exceptions for word length??
+
+# class Word:
+#   def __init__(self):
+#     print('init method')
+  
+#   def __enter__(self):
+#     print('entering word file')
+
+#   def __exit__(self, exc_info):
+#     print('exit words.txt')
+
+#   with open('words.txt', 'r') as f:
+#     words = f.read()
