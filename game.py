21e68003a13dfbc62b72dfe8da886a47dce256c7
Kat <kmneil2@gmail.com>
2020-02-22 10:54:34 -0500

    Add functions for reveal letters, duplicating print error

--- game.py
+++ game.py
@@ -1,19 +1,13 @@
 
 import random
 
-# class Words:
-
-#   def __init__(self, word):
-#     self.word = word 
-
-#   def __str__(self):
-#     word = print(random.choice(words))
 
 class Game:
   def __init__(self):
     self.player = Player('player1')
     self.word = self.choose_word()
-    self.blanks = self.start_game(self.word)
+    self.blanks = self.start_game()
+    self.guesses_left = 8
 
   def choose_word(self):
     with open('words.txt', 'r') as f:
@@ -26,31 +20,38 @@ class Game:
     # while len(s)>0:
     #     s.remove(random.choice(list(s)))
 
-  # def blank_word(self, blanks):
-  #   self.blanks = list('_'*len(self.word.strip()))
-  #   # print(self.blanks)
-  #   # return "Game has started. Your word is:", f"{self.blanks}"
-  #   return
-  
-  def start_game(self, blanks):
+  def start_game(self):
+    # self.blanks = self.word
     choice = input (f'Hello, {self.player.name}. Are you ready to play? (y)/(n)')
     if choice == 'y': 
       self.blanks = list('_'*len(self.word.strip()))
       print(self.blanks)
       return "Game has started. Your word is:", f"{self.blanks}"
-    
-  
-  
-  # def show_word_prompt(self, word):
-  #   print("Game has started. Your word is:", f"{blank_word}")
-   
-  
-  def guess_letter(self):
-    guess = input('What letter would you like to guess?')
+    elif input == 'n':
+      return "See you later..."
 
+  def ask_letter(self, char):
+    choice = input('What letter would you like to guess?')
+    if choice == len(char) > 1:
+      print ('Please choose one letter!')
 
-  # def play(self):
+  def reveal_letters(self, char):
+    if char in self.word:
+      while char in self.word:
+        i = self.word.index(char)
+        self.blanks[i] = char
+        self.word[i] = ""
+      if len("".join(self.word)) == 0:
+        return "You Win!"
+      else:
+        self.guesses_left -= 1
+        if self.guesses_left < 0:
+          return ("Game Over!")
 
+  # def guesses_left(self):
+  #   self.guesses_left -= 1
+  #   if self.guesses_left < 0:
+  #     return ("Game Over!")
 
 
 class Player:
@@ -65,23 +66,3 @@ class Player:
   
 game = Game()
 game.start_game()
-
-
-
-
-
-
-# Can I use this to add exceptions for word length??
-
-# class Word:
-#   def __init__(self):
-#     print('init method')
-  
-#   def __enter__(self):
-#     print('entering word file')
-
-#   def __exit__(self, exc_info):
-#     print('exit words.txt')
-
-#   with open('words.txt', 'r') as f:
-#     words = f.read()
